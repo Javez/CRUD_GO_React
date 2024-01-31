218c90a3c344edb85ec67458f8fda562c996d31c
Алексей Данилов <mr.javez@ukr.net>
2024-01-31 16:50:20 +0200

    Changes: -remove and add new Readme file -add backend with golang

--- backend/main/main.go
+++ backend/main/main.go
@@ -1,7 +1,163 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"time"
+	"fmt"
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// User represents the User model
+type User struct {
+	ID       string `json:"id" bson:"_id"`
+	Username string `json:"username" bson:"username"`
+	Email    string `json:"email" bson:"email"`
+}
+
+var client *mongo.Client
+var collection *mongo.Collection
+
+func init() {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().
+		ApplyURI("Dont look at my mongo key >-.-<").
+		SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	var err error
+	client, err = mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	// Set up the collection
+	collection = client.Database("testdb").Collection("users")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+ 	r.Use(cors.Default())
+
+	r.GET("/users", getUsers)
+	r.GET("/users/:id", getUser)
+	r.POST("/users", createUser)
+	r.PUT("/users/:id", updateUser)
+	r.DELETE("/users/:id", deleteUser)
+
+	// Run the server
+	r.Run(":8080")
+}
+
+func getUsers(c *gin.Context) {
+	var users []User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var user User
+		if err := cursor.Decode(&user); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		users = append(users, user)
+	}
+
+	c.JSON(http.StatusOK, users)
+}
+
+func getUser(c *gin.Context) {
+	id := c.Param("id")
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := collection.FindOne(ctx, bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func createUser(c *gin.Context) {
+	 var user User
+    if err := c.ShouldBindJSON(&user); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    result, err := collection.InsertOne(ctx, user)
+    if err != nil {
+        // Log the error for debugging
+        log.Println("Error creating user:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+        return
+    }
+
+    user.ID = result.InsertedID.(string)
+    c.JSON(http.StatusCreated, user)
+}
+
+func updateUser(c *gin.Context) {
+	id := c.Param("id")
+
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := collection.UpdateOne(ctx, bson.M{"_id": id}, bson.M{"$set": user})
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func deleteUser(c *gin.Context) {
+	id := c.Param("id")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := collection.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
 }
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Getting Started with Simple CRUD Go and React app.
+
+Runs the app in the development mode.
+Open [http://localhost:3000](http://localhost:3000) to view frontend.\
+Open Postman [http://localhost:8080](http://localhost:8080) to make requests to backend.
+
+## Setup Scripts
+- Frontend: `npx create-react-app user-frontend`
+- Backend: `just with your hands`
+
+
+## Run Scripts
+In the frontend directory, you can run next command to start app:
+- `npm run start`\
+In the backend main directory, you can run next command to start app:
+- `go run main.go`
+
+## Packages
+**Note: This is the list of `packages`, for backend!**
+- `npm install axios`
+
+**Note: This is the list of `packages`, for backend!**
+- `go get -u github.com/gin-gonic/gin`
+- `go get -u go.mongodb.org/mongo-driver/mongo`
+
+## Learn More
+
+If you aren't satisfied im very sad :d
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+You can learn more in the [Create Go App documentation](https://go.dev/doc/tutorial/getting-started).
+
